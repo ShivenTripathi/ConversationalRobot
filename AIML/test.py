a5a706bf01d162402223367b6b9859e352ffa67c
catchmeifyocan <shiven@iitk.ac.in>
2020-05-13 19:52:21 +0530

    restructuring, database figured out

--- AIML/test.py
+++ AIML/test.py
@@ -1,6 +1,25 @@
 import aiml
+import os
+os. chdir("/home/shiven/Desktop/ConversationalRobot/AIML/data/")
+register=open('register.txt','r')
 kernel=aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
+message=input("Hello this is Alice, a sales bot, Are you a new customer?(y/n)")
+if message=='y':
+    id=0
+    for x in register:
+        id+=1
+    register.close()
+    register = open("register.txt","w")#write mode 
+    register.write(str(id)+"\n") 
+    sess_file=open(str(id)+'.txt','a')
+else:
+    message=input("Enter Customer id")
+    sess_file=open(str(message)+'.txt','a')
+register.close()
 while True:
-    print(kernel.respond(input("Enter your message >> ")))
+    message=input("Enter your message >> ")
+    output=kernel.respond(message)
+    print(output)
+sess_file.close()
